997ba4bf88cec17ad9a0c80c834fc8a0e6c8b885
ayush14029 <ayush14029@iiitd.ac.in>
2015-09-13 15:58:09 +0530

    ApplicantGUI
    This is the first GUI and its controller along with the main class

---
+++ APCourseProject/src/apcourseproject/ApplicantController.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apcourseproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+/**
+ *
+ * @author Ayush
+ */
+public class ApplicantController implements Initializable {
+    @FXML
+    private Hyperlink linkcontact;
+    @FXML
+    private Hyperlink linkfaq;
+    @FXML
+    private Tab tab1;
+    @FXML
+    private ScrollPane scroll1;
+    @FXML
+    private Label t1lab2;
+    @FXML
+    private Label t1lab1;
+    @FXML
+    private Label t1lab5;
+    @FXML
+    private Label t1lab3;
+    @FXML
+    private Label t1lab4;
+    @FXML
+    private TextField t1text3;
+    @FXML
+    private TextField t1text5;
+    @FXML
+    private TextField t1text1;
+    @FXML
+    private TextField t1text4;
+    @FXML
+    private TextField t1text2;
+    @FXML
+    private Label t1lab6;
+    @FXML
+    private RadioButton t1rb1;
+    @FXML
+    private ToggleGroup t1tg1;
+    @FXML
+    private RadioButton t1rb3;
+    @FXML
+    private RadioButton t1rb2;
+    @FXML
+    private Label t1lab7;
+    @FXML
+    private TextField t1text6;
+    @FXML
+    private TextField t1text7;
+    @FXML
+    private TextField t1text8;
+    @FXML
+    private Label t1lab8;
+    @FXML
+    private RadioButton t1rb4;
+    @FXML
+    private ToggleGroup t1tg2;
+    @FXML
+    private RadioButton t1rb5;
+    @FXML
+    private RadioButton t1rb6;
+    @FXML
+    private ToggleGroup t1tg3;
+    @FXML
+    private RadioButton t1rb7;
+    @FXML
+    private RadioButton t1rb10;
+    @FXML
+    private ToggleGroup t1tg4;
+    @FXML
+    private RadioButton t1rb11;
+    @FXML
+    private DatePicker t1dp;
+    @FXML
+    private RadioButton t1rb12;
+    @FXML
+    private ToggleGroup t1tg5;
+    @FXML
+    private RadioButton t1rb13;
+    @FXML
+    private RadioButton t1rb8;
+    @FXML
+    private RadioButton t1rb9;
+    @FXML
+    private TextField t1text9;
+    @FXML
+    private TextField t1text10;
+    @FXML
+    private TextField t1text11;
+    @FXML
+    private TextField t1text12;
+    @FXML
+    private TextField t2text1;
+    @FXML
+    private TextField t2text2;
+    @FXML
+    private TextField t2text3;
+    @FXML
+    private TextField t2text4;
+    @FXML
+    private TextField t2text5;
+    @FXML
+    private TextField t2text6;
+    @FXML
+    private TextField t2text7;
+    @FXML
+    private TextField t2text8;
+    @FXML
+    private TextField t2text9;
+    @FXML
+    private TextField t2text10;
+    @FXML
+    private TextField t2text11;
+    @FXML
+    private TextField t2text12;
+    @FXML
+    private TextField t2text13;
+    @FXML
+    private RadioButton t2rb1;
+    @FXML
+    private RadioButton t2rb2;
+    @FXML
+    private TextField t2text14;
+    @FXML
+    private TextField t2text15;
+    @FXML
+    private CheckBox t2cb3;
+    @FXML
+    private CheckBox t2cb4;
+    @FXML
+    private CheckBox t2cb1;
+    @FXML
+    private CheckBox t2cb2;
+    @FXML
+    private TextField t2text16;
+    @FXML
+    private Button t4btn1;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
